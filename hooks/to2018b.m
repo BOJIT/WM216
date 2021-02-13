b26302ccdaf22840ff69adb187352484c7ca14db
BOJIT <35117353+BOJIT@users.noreply.github.com>
2021-02-13 11:44:40 +0000

    testing git hook operation

---
+++ hooks/to2018b.m
@@ -0,0 +1,7 @@
+% DESCRIPTION:  Script called by Git hook to re-version simulink files.
+% AUTHOR:       James Bennion-Pedley
+% DATE CREATED: 13.02.21
+
+function to2018b(paths)
+    disp(paths);
+end
---
+++ hooks/to2018b.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Git hook for re-versioning simulink files on pre-commit.
+echo "Hello Git!"
