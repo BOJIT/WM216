b9d70a4eec5c858070c2ad7b12fc0cd53ca33ee1
BOJIT <35117353+BOJIT@users.noreply.github.com>
2021-02-13 11:49:52 +0000

    Add a bit more startup info + ignore locals file

--- hooks/startup.m
+++ hooks/startup.m
@@ -1,3 +1,24 @@
-% Startup script.
+% DESCRIPTION:  Startup script for initlialising project with
+%               forced version saving on commits.
+% AUTHOR:       James Bennion-Pedley
+% DATE CREATED: 13.02.21
 
-disp('Hello World!');
+
+% Create temporary file for Git to use in pre_commit hook.
+fd = fopen('hooks/locals', 'w');
+
+% Write MATLAB version to temp file.
+matlab_info = ver;
+fprintf(fd, [char(matlab_info(1).Release), '\n']);
+
+% Write MATLAB executable path to temp file.
+fprintf(fd, [fullfile(matlabroot, 'bin'), '\n']);
+
+% Copy shell script to .git/hooks directory.
+copyfile 'hooks/pre-commit' '.git/hooks/';
+
+% Close temp file.
+fclose(fd);
+
+% Confirm initialisation occurred correctly.
+disp('Project Initialised:');
