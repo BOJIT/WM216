be08980799ca4d37e5e7219364d913f47d05458b
fboonham <francesca.boonham@dyson.com>
2021-03-28 00:06:01 +0000

    Increasing ease of use of the file to explore R and L

--- src/2-2/speakerTesting.m
+++ src/2-2/speakerTesting.m
@@ -2,6 +2,7 @@ clc; clear;
 
 path = 'model_parameters.json';
 name = 'speakerModel';
+investigate = 'L'; %R for resistance, L for inductance
 
 
 % Get environment variables from JSON
@@ -14,8 +15,11 @@ for ws = param.workspace'
 end
 
 % Construct swept parameters
-sweep.R = linspace(8, 300, 10);
-% sweep.L = linspace(0.0002, 10, 10);
+if investigate == 'R'
+    sweep.R = linspace(8, 300, 10);
+elseif investigate == 'L'
+    sweep.L = linspace(0.0002, 10, 10);
+end
 
 % Process results
 sims = SimFramework(path, false, name, workspace, sweep);
@@ -29,4 +33,16 @@ grid on;
 hold on;
 xlabel('Time / (seconds)');
 ylabel(sims(1).yout{node}.Name);
-arrayfun(@(x) plot(x.yout{node}.Values), sims);                  
+arrayfun(@(x) plot(x.yout{node}.Values), sims);
+
+% Labels for graphs
+if investigate == 'R'
+    title('Effect of varying resistance R on output current I', 'FontSize',18,'interpreter', 'latex');
+    lgd.R = legend(string(round(sweep.R)),'Location','northwest');
+    title(lgd.R,'Resistance, R [Ohms]');
+elseif investigate == 'L'
+    title('Effect of varying inductance L on output current I', 'FontSize',18,'interpreter', 'latex');
+    xlim([0 2])
+    lgd.L = legend([string(round(sweep.L(1)*1000,1)),string(round(sweep.L(2:10)*1000))],'Location','northwest');
+    title(lgd.L,'Inductance, L [mH]');
+end
