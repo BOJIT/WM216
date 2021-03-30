a7165c3948b3be7ca5fe500f75b8c169c92c174e
fboonham <francesca.boonham@dyson.com>
2021-03-30 18:14:26 +0100

    Add coupling functionality to the speaker tester

--- src/2-2/speakerTesting.m
+++ src/2-2/speakerTesting.m
@@ -2,8 +2,9 @@ clc; clear;
 
 path = 'model_parameters.json';
 name = 'speakerModel';
-investigate = 'L'; %R for resistance, L for inductance
-
+var_investigate = 'R'; %R for resistance, L for inductance
+var_output = 1; %1 for current, 2 for displacement (value on the y axis)
+var_coupled = 0; %1 for coupled, 0 for uncoupled
 
 % Get environment variables from JSON
 json = fileread(path);
@@ -14,10 +15,13 @@ for ws = param.workspace'
     end
 end
 
+% Adjust coupling setting
+workspace.couple = var_coupled;
+
 % Construct swept parameters
-if investigate == 'R'
+if var_investigate == 'R'
     sweep.R = linspace(8, 300, 10);
-elseif investigate == 'L'
+elseif var_investigate == 'L'
     sweep.L = linspace(0.0002, 10, 10);
 end
 
@@ -26,7 +30,7 @@ sims = SimFramework(path, false, name, workspace, sweep);
 
 % Plot overlayed results
 
-node = 1; % 1 for elec, 2 for mech
+node = var_output;
 
 figure;
 grid on;
@@ -36,13 +40,13 @@ ylabel(sims(1).yout{node}.Name);
 arrayfun(@(x) plot(x.yout{node}.Values), sims);
 
 % Labels for graphs
-if investigate == 'R'
+if var_investigate == 'R'
     title('Effect of varying resistance R on output current I', 'FontSize',18,'interpreter', 'latex');
-    lgd.R = legend(string(round(sweep.R)),'Location','northwest');
+    lgd.R = legend(string(round(sweep.R)),'Location','northeast');
     title(lgd.R,'Resistance, R [Ohms]');
-elseif investigate == 'L'
+elseif var_investigate == 'L'
     title('Effect of varying inductance L on output current I', 'FontSize',18,'interpreter', 'latex');
     xlim([0 2])
-    lgd.L = legend([string(round(sweep.L(1)*1000,1)),string(round(sweep.L(2:10)*1000))],'Location','northwest');
+    lgd.L = legend([string(round(sweep.L(1)*1000,1)),string(round(sweep.L(2:10)*1000))],'Location','northeast');
     title(lgd.L,'Inductance, L [mH]');
 end
